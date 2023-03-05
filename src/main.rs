6cf00f5281a395c8b8d0b9fb399985df5883cbe3
sylvain <sylvain.ecuer@epita.fr>
2023-03-05 21:47:51 +0100

    linear_impulse_resolution

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
+#![recursion_limit = "20000"]
+
 use std::{
-    borrow::BorrowMut,
+    borrow::{Borrow, BorrowMut},
     cell::RefCell,
     cmp::{self, max, min},
+    ops::{Deref, DerefMut},
     rc::Rc,
+    sync::Mutex,
 };
 
+use generational_arena::Arena;
 use raylib::prelude::*;
 
-const HEIGHT: usize = 480;
-const WIDTH: usize = 640;
+const HEIGHT: isize = 480;
+const WIDTH: isize = 640;
 const GRAVITY: f32 = 2.0;
 const BALL_RADIUS: f32 = 10.0;
-const FRICTION: f32 = 0.95;
-const ACCELERATION: f32 = 0.1;
+const FRICTION: f32 = 0.90;
+const ACCELERATION: f32 = 0.5;
+const MAX_SPEED: f32 = 20.0;
+const BALL_MASS: f32 = 1.0;
+const BALL_COEFF_RESTITUTION: f32 = 0.8;
+
 
 fn main() {
     let (mut rl, thread) = raylib::init()
@@ -21,38 +30,26 @@ fn main() {
         .build();
 
     // limit the framerate to 60
-    rl.set_target_fps(10);
+    rl.set_target_fps(30);
 
-    let mut matrix: Vec<Vec<Option<Ball>>> = vec![];
+    let mut arena: Arena<Ball> = Arena::new();
 
-    // initialize rows
-
-    for _ in 0..((HEIGHT as f32 / BALL_RADIUS) as usize) {
-        let mut row: Vec<Option<Ball>> = vec![];
-        for _ in 0..((WIDTH as f32 / BALL_RADIUS) as usize) {
-            row.push(None);
-        }
-        matrix.push(row);
-    }
-
-    // create a game state
-    let grid: Grid = Grid {
-        width: WIDTH,
-        height: HEIGHT,
-        rows: (HEIGHT as f32 / BALL_RADIUS) as usize,
-        cols: (WIDTH as f32 / BALL_RADIUS) as usize,
-        spacing: 10.0,
-        // create an empty matrix with rows= (HEIGHT as f32 / BALL_RADIUS) & cols= (WIDTH as f32 / BALL_RADIUS)
-        // fill the matric of None
-        matrix: matrix,
-    };
+    let mut color_generator = ColorGenerator::new();
 
     let mut game_state = GameState {
         gravity: GRAVITY,
-        grid: grid,
         moves: vec![],
+        arena: arena,
         collisions: vec![],
+        balls: vec![],
+        color_generator: color_generator,
     };
+    game_state.add_ball(
+        (250.0 + 0 as f32) % WIDTH as f32,
+        50.0 + (0 % 2) as f32,
+        BALL_RADIUS,
+        0,
+    );
 
     let mut ctx = 0;
 
@@ -63,18 +60,15 @@ fn main() {
         ctx = ctx + 1;
 
         game_state
-            .apply_gravity()
-            .update_position()
+        .add_ball(
+            (250.0 + ctx as f32) % WIDTH as f32,
+            55.0 + (ctx % 2) as f32,
+            BALL_RADIUS,
+            ctx,
+        )
             .check_for_collisions()
-            .apply_collisions()
-            .apply_moves()
-            .add_ball(
-                (250.0 + ctx as f32) % WIDTH as f32,
-                50.0 + (ctx % 2) as f32,
-                BALL_RADIUS,
-                Color::RED,
-                ctx,
-            )
+            .linear_impulse_resolution()
+            .apply_movement()
             .draw_balls(&mut d);
 
         //game_state.monitor_drift();
@@ -84,9 +78,39 @@ fn main() {
 // make a struct for the game state
 struct GameState {
     gravity: f32,
-    grid: Grid,
     moves: Vec<Move>,
-    collisions: Vec<(Ball, Ball)>,
+    arena: Arena<Ball>,
+    collisions: Vec<(generational_arena::Index, generational_arena::Index)>,
+    balls: Vec<Rc<Ball>>,
+    color_generator: ColorGenerator,
+}
+
+struct ColorGenerator {
+    index: u8,
+}
+
+impl ColorGenerator {
+    fn new() -> ColorGenerator {
+        ColorGenerator { index: 0 }
+    }
+
+    fn consumme(&mut self) -> Color {
+        self.index = ((self.index as usize + 11) % 254) as u8;
+        if (self.index as usize) > 255 {
+            self.index = 0;
+        }
+        let c = match self.index % 6 {
+            0 => Color::new(255, 0, self.index, 255),
+            1 => Color::new(255, self.index, self.index, 255),
+            2 => Color::new(self.index, 255, 0, 255),
+            3 => Color::new(0, 255, self.index, 255),
+            4 => Color::new(0, self.index, 255, 255),
+            5 => Color::new(self.index, 0, 255, 255),
+            _ => Color::new(0, 0, 0, 255),
+        };
+
+        return c;
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -95,13 +119,29 @@ struct Ball {
     y: f32,
     radius: f32,
     color: Color,
-    movement: Movement,
+    velocity: Velocity,
+    mass: f32,
+    coeff_restitution: f32,
 }
 
 #[derive(Debug, Copy, Clone)]
-struct Movement {
+struct Velocity {
     x: f32,
     y: f32,
+}
+
+impl Velocity {
+    fn relative_velocity(&self, other: &Velocity) -> Velocity {
+        return Velocity {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        };
+    }
+
+    fn nomal(&self) -> f32 {
+        let norm = -1.0;
+        return norm;
+    }
 }
 
 impl Ball {
@@ -114,230 +154,170 @@ impl Ball {
 }
 
 impl GameState {
-    fn add_ball(&mut self, x: f32, y: f32, radius: f32, color: Color, ctx: i32) -> &mut GameState {
+    fn add_ball(&mut self, x: f32, y: f32, radius: f32, ctx: i32) -> &mut GameState {
         if ctx % 10 != 0 {
             return self;
         }
-        self.grid.matrix[0][0] = Some(Ball {
+        let color = self.color_generator.consumme();
+        self.arena.insert(Ball {
             x: x,
             y: y,
             radius: radius,
             color: color,
-            movement: Movement { x: 0.0, y: 0.0 },
+            velocity: Velocity { x: 5.0, y: 0.0 },
+            mass: BALL_MASS,
+            coeff_restitution: BALL_COEFF_RESTITUTION,
         });
-
         return self;
     }
 
-    fn apply_gravity(&mut self) -> &mut GameState {
-        for vec in &mut self.grid.matrix {
-            for ball_option in vec {
-                match ball_option {
-                    Some(ball) => {
-                        // check for out of bounds
-                        if ball.y + ball.radius >= self.grid.height as f32 {
-                            continue;
-                        }
-                        // ball.movement.y += self.gravity;
-                        // ball.y += ball.movement.y * ACCELERATION;
-                        // if (ball.y + ball.radius) >= (self.grid.height - 1) as f32 {
-                        //     ball.y = self.grid.height as f32 - ball.radius;
-                        //     ball.movement.y = 0.0;
-                        // }
-                    }
-                    None => (),
-                }
+    fn apply_movement(&mut self) -> &mut GameState {
+        for (idx, ball) in self.arena.iter_mut() {
+            let ball_y = ball.y as isize;
+            let ball_x = ball.x as isize;
+            let ball_radius = ball.radius as isize;
+
+            // limit the speed to avoid the ball from going through the walls
+            if ball.velocity.y > 10.0 {
+                ball.velocity.y = 9.0;
             }
-        }
-        return self;
-    }
-
-    fn update_position(&mut self) -> &mut GameState {
-        // update the position of the ball and put them in the corresponding cell
-        self.moves.clear();
-        for (idx, vec) in self.grid.matrix.iter().enumerate() {
-            // get the indices of the ball
-            for (jdx, ball_option) in vec.iter().enumerate() {
-                match ball_option {
-                    Some(ball) => {
-                        let row = (ball.y / BALL_RADIUS) as isize;
-                        let col = (ball.x / BALL_RADIUS) as isize;
-
-                        // check if the ball is out of the grid
-                        if row >= self.grid.rows as isize
-                            || col >= self.grid.cols as isize
-                            || row < 0
-                            || col < 0
-                        {
-                            println!("Ball out of bounds");
-                            continue;
-                        }
-
-                        // check if the cell is empty
-                        if self.grid.matrix[row as usize][col as usize].is_none() {
-                            self.moves.push(Move {
-                                from: (idx, jdx),
-                                to: (row as usize, col as usize),
-                            });
-                        }
-                    }
-                    None => (),
-                }
+            if ball.velocity.y < -10.0 {
+                ball.velocity.y = -9.0;
             }
+            if ball.velocity.x > 10.0 {
+                ball.velocity.x = 9.0;
+            }
+            if ball.velocity.x < -10.0 {
+                ball.velocity.x = -9.0;
+            }
+
+            if (ball_y +  ball_radius) >= HEIGHT {
+                ball.velocity.y = - (ball.velocity.y).abs() * BALL_COEFF_RESTITUTION;
+            } else if (ball_y -  ball_radius) < 0 {
+                ball.velocity.y = (ball.velocity.y).abs() * BALL_COEFF_RESTITUTION;
+            } else {
+                ball.velocity.y += self.gravity * ACCELERATION;
+            }
+
+            // apply gravity and friction to the ball
+
+            ball.y += (ball.velocity.y * FRICTION);
+
+            if ball_x  +  ball_radius > WIDTH {
+                ball.velocity.x = -ball.velocity.x.abs() * BALL_COEFF_RESTITUTION;
+            } else if ball_x  -  ball_radius < 0 {
+                ball.velocity.x = ball.velocity.x.abs() * BALL_COEFF_RESTITUTION;
+            }
+            // check for overflow and underflow
+            ball.x += ball.velocity.x * FRICTION;
         }
         return self;
     }
 
     fn draw_balls(&mut self, d: &mut RaylibDrawHandle) -> &mut GameState {
-        for vec in &self.grid.matrix {
-            for ball_option in vec {
-                match ball_option {
-                    Some(ball) => {
-                        d.draw_circle(ball.x as i32, ball.y as i32, ball.radius, ball.color)
-                    }
-                    None => (),
-                }
-            }
+        for (idx, ball) in self.arena.iter() {
+            d.draw_circle_v(Vector2::new(ball.x, ball.y), ball.radius, ball.color);
         }
         return self;
-    }
-
-    fn apply_moves(&mut self) -> &mut GameState {
-        for mov in &self.moves {
-            let (from_row, from_col) = mov.from;
-            let (to_row, to_col) = mov.to;
-
-            let rest = std::mem::replace(&mut self.grid.matrix[from_row][from_col], None);
-            self.grid.matrix[to_row][to_col] = rest;
-        }
-        self.moves.clear();
-        return self;
-    }
-
-    fn monitor_drift(&self) {
-        // print the values of the balls and their grid position to check for drift
-        for (idx, vec) in self.grid.matrix.iter().enumerate() {
-            for (jdx, ball_option) in vec.iter().enumerate() {
-                match ball_option {
-                    Some(ball) => {
-                        println!("drifto: {} {} {}", ball.debug(), idx, jdx);
-                    }
-                    None => (),
-                }
-            }
-        }
     }
 
     fn check_for_collisions(&mut self) -> &mut GameState {
-        self.collisions.clear();
-        for (idx, vec) in (&self.grid.matrix).iter().enumerate() {
-            // get the indices of the ball
-            for (jdx, ball_option) in vec.iter().enumerate() {
-                match ball_option {
-                    Some(ball) => {
-                        let row = (ball.y / BALL_RADIUS) as isize;
-                        let col = (ball.x / BALL_RADIUS) as isize;
-
-                        // check the adjacent cells
-                        for i in -1..2 {
-                            for j in -1..2 {
-                                let new_row = row + i;
-                                let new_col = col + j;
-
-                                if new_row < 0 || new_col < 0 {
-                                    continue;
-                                }
-
-                                let new_row = new_row as usize;
-                                let new_col = new_col as usize;
-
-                                if new_row >= self.grid.rows - 1 || new_col >= self.grid.cols - 1 {
-                                    continue;
-                                }
-                                match &self.grid.matrix[new_row][new_col] {
-                                    Some(other_ball) => {
-                                        if ball.y > other_ball.y || ball.x > other_ball.x {
-                                            continue;
-                                        }
-                                        if (ball.x, ball.y) != (other_ball.x, other_ball.y) {
-                                            let distance = ((ball.x - other_ball.x).powi(2)
-                                                + (ball.y - other_ball.y).powi(2))
-                                            .sqrt();
-                                            if distance <= (2.0 * BALL_RADIUS) {
-                                                println!(
-                                                    "added collision for {} and {}",
-                                                    ball.debug(),
-                                                    other_ball.debug()
-                                                );
-                                                self.collisions.push((*ball, *other_ball));
-                                            }
-                                        }
-                                    }
-
-                                    _ => (),
-                                }
-                            }
-                        }
-                    }
-                    _ => (),
+        for (idx, ball) in self.arena.iter() {
+            for (jdx, ball2) in self.arena.iter() {
+                if idx == jdx || jdx > idx {
+                    continue;
+                }
+                let distance = ((ball.x - ball2.x).powi(2) + (ball.y - ball2.y).powi(2)).sqrt();
+                if distance <= (2.0 * BALL_RADIUS) {
+                    self.collisions.push((idx, jdx));
                 }
             }
         }
-        println!("collisions: {:?}", self.collisions);
-        println!("===================");
+        return self;
+    }
+
+    fn linear_impulse_resolution(&mut self) -> &mut GameState {
+        for (idx, jdx) in &self.collisions {
+            let (ball_a, ball_b) = self.arena.get2_mut(*idx, *jdx);
+
+            let mut ball_a = ball_a.unwrap();
+            let mut ball_b = ball_b.unwrap();
+
+            let e = ball_a.coeff_restitution.min(ball_b.coeff_restitution);
+            let numerator =
+                -(1.0 + e) * ball_a.velocity.relative_velocity(&ball_b.velocity).nomal();
+            let denominator = 1.0 / ball_a.mass + 1.0 / ball_b.mass;
+            let impulse = numerator / denominator;
+            let impulse = impulse / 2.0 ;
+            ball_a.velocity.x += impulse / ball_a.mass * -1.0;
+            ball_b.velocity.x += impulse / ball_b.mass * 1.0;
+
+            ball_a.velocity.y += impulse / ball_a.mass * -1.0;
+            ball_b.velocity.y += impulse / ball_b.mass * 1.0;
+        }
+        self.collisions.clear();
+
         return self;
     }
 
     fn apply_collisions(&mut self) -> &mut GameState {
-        for (ball, other_ball) in &self.collisions {
-            // move the ball away from each center
-            let distance = ((ball.x - other_ball.x).powi(2) + (ball.y - other_ball.y).powi(2)).sqrt();
+        for (idx, jdx) in &self.collisions {
+            let (ball_a, ball_b) = self.arena.get2_mut(*idx, *jdx);
 
-            let x_diff = (ball.x - other_ball.x);
-            let y_diff = (ball.y - other_ball.y);
+            let mut ball_a = ball_a.unwrap();
+            let mut ball_b = ball_b.unwrap();
 
-            let mut mut_ball = match self.grid.matrix[(ball.y / BALL_RADIUS) as usize]
-                [(ball.x / BALL_RADIUS) as usize]
-                .as_mut()
-            {
-                Some(ball) => ball,
-                None => continue,
-            };
+            let x = ball_a.x - ball_b.x;
+            let y = ball_a.y - ball_b.y;
 
-            println!("{}", x_diff);
+            let x = 2.0 * BALL_RADIUS - x.abs();
+            let y = 2.0 * BALL_RADIUS - y.abs();
 
-            if ball.x >= other_ball.x {
-                mut_ball.x = (ball.x + x_diff);
+            // if ball_b is higher than ball_a
+            if ball_a.y > ball_b.y {
+                ball_b.y -= (y / 2.0);
             } else {
-                mut_ball.x = (ball.x - x_diff);
+                ball_a.y -= (y / 2.0);
+                ball_b.y += (y / 2.0);
             }
 
-            if ball.y >= other_ball.y {
-                mut_ball.y = (ball.y - y_diff);
+            // if ball_b is to the right of ball_a
+            if ball_a.x > ball_b.x {
+                ball_b.x -= (x / 2.0);
+                ball_a.y += (x / 2.0);
             } else {
-                mut_ball.y = (ball.y + y_diff);
+                ball_a.y -= (x / 2.0);
+                ball_b.x += (x / 2.0);
             }
 
-            let mut other_ball = match self.grid.matrix[(other_ball.y / BALL_RADIUS) as usize]
-                [(other_ball.x / BALL_RADIUS) as usize]
-                .as_mut()
-            {
-                Some(other_ball) => other_ball,
-                None => continue,
-            };
-
-            if ball.x >= other_ball.x {
-                other_ball.x = (other_ball.x - x_diff);
-            } else {
-                other_ball.x = (other_ball.x + x_diff);
+            if (ball_a.x + ball_a.radius) >= WIDTH as f32 {
+                ball_a.x = (WIDTH as f32 - ball_a.radius) as f32;
+            }
+            if (ball_a.y + ball_a.radius) >= HEIGHT as f32 {
+                ball_a.y = (HEIGHT as f32 - ball_a.radius) as f32;
+            }
+            if (ball_b.x + ball_b.radius) >= WIDTH as f32 {
+                ball_b.x = (WIDTH as f32 - ball_b.radius) as f32;
+            }
+            if (ball_b.y + ball_b.radius) >= HEIGHT as f32 {
+                ball_b.y = (HEIGHT as f32 - ball_b.radius) as f32;
+            }
+            if (ball_a.x - ball_a.radius) <= 0.0 {
+                ball_a.x = ball_a.radius;
+            }
+            if (ball_a.y - ball_a.radius) <= 0.0 {
+                ball_a.y = ball_a.radius;
+            }
+            if (ball_b.x - ball_b.radius) <= 0.0 {
+                ball_b.x = ball_b.radius;
+            }
+            if (ball_b.y - ball_b.radius) <= 0.0 {
+                ball_b.y = ball_b.radius;
             }
 
-            if ball.y >= other_ball.y {
-                other_ball.y = (other_ball.y + y_diff);
-            } else {
-                other_ball.y = (other_ball.y - y_diff);
-            }
+            // make them move away from each other
         }
+        self.collisions.clear();
         return self;
     }
 }
@@ -351,7 +331,6 @@ struct Grid {
     cols: usize,
     spacing: f32,
     // make the matrix take an option of a reference of ball
-    matrix: Vec<Vec<Option<Ball>>>,
 }
 
 #[derive(Debug)]
